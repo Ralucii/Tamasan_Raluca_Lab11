b12d7dee528c2942d4a4a474a9223a8d6a6efcbd
ralucoita <ralucaatamasan@yahoo.ro>
2023-01-10 19:38:37 +0200

    Add project files.

---
+++ Tamasan_Raluca_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tamasan_Raluca_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tamasan_Raluca_Lab11/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Tamasan_Raluca_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
